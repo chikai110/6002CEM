f62460b313f5ca6e2ba4b5c51c5ab2c6a6ced2ba
= <=>
2022-06-06 23:30:36 +0800

    Added Biometric Login function

--- app/src/main/java/com/example/a6002cem/LoginFragment.kt
+++ app/src/main/java/com/example/a6002cem/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.a6002cem
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.TextUtils
 import androidx.fragment.app.Fragment
@@ -8,16 +9,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.concurrent.Executor
 
 class LoginFragment : Fragment() {
     private lateinit var username: EditText
     private lateinit var password: EditText
     private lateinit var fAuth: FirebaseAuth
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
 
     override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +48,62 @@ class LoginFragment : Fragment() {
 
         view.findViewById<Button>(R.id.btn_login).setOnClickListener {
             validateForm()
+        }
+
+        executor = ContextCompat.getMainExecutor(requireContext())
+
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationError(errorCode: Int,
+                                                   errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(requireContext(),
+                        "Authentication error: $errString", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    var navHome = activity as FragmentNavigation
+                    navHome.navigateFrag(HomeFragment(),addToStack = true)
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(requireContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        val biometricLoginButton =
+            view.findViewById<Button>(R.id.biometric_login)
+        biometricLoginButton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+
+        val biometricStatusTextView =
+            view.findViewById<TextView>(R.id.biometric_status)
+        val biometricManager = BiometricManager.from(requireContext())
+        when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG or BiometricManager.Authenticators.DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                biometricStatusTextView.text = "App can authenticate using biometrics."
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                biometricStatusTextView.text = "No biometric features available on this device."
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                biometricStatusTextView.text = "Biometric features are currently unavailable."
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                // Prompts the user to create credentials that your app accepts.
+                biometricStatusTextView.text = "Biometric features are not enrolled."
         }
 
         return view
