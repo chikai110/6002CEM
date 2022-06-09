41374902f332557ca4bb6981eb8d916dcc1aef2c
= <=>
2022-06-09 11:38:08 +0800

    Updated Location Fragment

--- app/src/main/java/com/example/a6002cem/LocationFragment.kt
+++ app/src/main/java/com/example/a6002cem/LocationFragment.kt
@@ -35,8 +35,7 @@ class LocationFragment : Fragment() {
     // member views
     private var mLatitudeText: TextView? = null
     private var mLongitudeText: TextView? = null
-    private var mTimeText: TextView? = null
-    private var mOutput: TextView? = null
+    private var mLocationText: TextView? = null
     private var mLocateButton: Button? = null
 
     // member variables that hold location info
@@ -44,8 +43,6 @@ class LocationFragment : Fragment() {
     private var mLocationRequest: LocationRequest? = null
     private var mGeocoder: Geocoder? = null
     private var mLocationProvider: FusedLocationProviderClient? = null
-
-    private lateinit var mMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,15 +53,14 @@ class LocationFragment : Fragment() {
 
         mLatitudeText = view.findViewById<View>(R.id.latitude_text) as TextView
         mLongitudeText = view.findViewById<View>(R.id.longitude_text) as TextView
-        mTimeText = view.findViewById<View>(R.id.time_text) as TextView
+        mLocationText = view.findViewById<View>(R.id.current_location) as TextView
         mLocateButton = view.findViewById<View>(R.id.locate) as Button
-        mOutput = view.findViewById<View>(R.id.output) as TextView
 
         // below are placeholder values used when the app doesn't have the permission
         mLatitudeText!!.text = "Latitude not available yet"
         mLongitudeText!!.text = "Longitude not available yet"
-        mTimeText!!.text = "Time not available yet"
-        mOutput!!.text = ""
+        mLocationText!!.text = ""
+
         val locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions(),
             ActivityResultCallback<Map<String?, Boolean?>> { result: Map<String?, Boolean?> ->
@@ -77,15 +73,15 @@ class LocationFragment : Fragment() {
                 if (fineLocationGranted != null && fineLocationGranted) {
                     // Precise location access granted.
                     // permissionOk = true;
-                    mTimeText!!.text = "permission granted"
+                    mLocationText!!.text = "permission granted"
                 } else if (coarseLocationGranted != null && coarseLocationGranted) {
                     // Only approximate location access granted.
                     // permissionOk = true;
-                    mTimeText!!.text = "permission granted"
+                    mLocationText!!.text = "permission granted"
                 } else {
                     // permissionOk = false;
                     // No location access granted.
-                    mTimeText!!.text = "permission not granted"
+                    mLocationText!!.text = "permission not granted"
                 }
             }
         )
@@ -119,7 +115,7 @@ class LocationFragment : Fragment() {
             supportMapFragment!!.getMapAsync { googleMap ->
                 // Initialize marker options
                 var markerOptionsList = ArrayList<MarkerOptions>()
-                markerOptionsList.add(MarkerOptions().position(currentLocation).title(mOutput!!.text as String?))
+                markerOptionsList.add(MarkerOptions().position(currentLocation).title(mLocationText!!.text as String?))
                 markerOptionsList.add(MarkerOptions().position(cinema1).title("Paris London New York Milano Cinema"))
                 markerOptionsList.add(MarkerOptions().position(cinema2).title("Hyland Theatre"))
                 // Remove all marker
@@ -137,7 +133,7 @@ class LocationFragment : Fragment() {
 
     private fun onStartClicked (v : View) {
         mLocationProvider = activity?.let { LocationServices.getFusedLocationProviderClient(it) }
-        mTimeText!!.text = "Started updating location"
+        mLocationText!!.text = "Started updating location"
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -177,14 +173,14 @@ class LocationFragment : Fragment() {
                 } else {
                     addressLines.append(address.getAddressLine(0))
                 }
-                mOutput!!.text = addressLines
+                mLocationText!!.text = addressLines
                 sharedPreferences = requireActivity().getSharedPreferences("SharedPreMain", Context.MODE_PRIVATE)
                 val editor = sharedPreferences!!.edit()
                 editor.putString(MainActivity.CURRENT_LOCATION, addresses[0].countryName.toString())
                 editor.commit()
 
             } else {
-                mOutput!!.text = "WARNING! Geocoder returned more than 1 addresses!"
+                mLocationText!!.text = "WARNING! Geocoder returned more than 1 addresses!"
             }
         } catch (e: Exception) {
         }
@@ -195,7 +191,6 @@ class LocationFragment : Fragment() {
             mLastLocation = result.lastLocation
             mLatitudeText!!.text = mLastLocation!!.latitude.toString()
             mLongitudeText!!.text = mLastLocation!!.longitude.toString()
-            mTimeText!!.text = DateFormat.getTimeInstance().format(Date())
         }
     }
 
